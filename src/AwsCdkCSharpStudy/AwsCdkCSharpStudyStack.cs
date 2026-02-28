66a1e2903ae30806736b55b2d019e232826a08e3
Rishav  A. <64297566+Ris345@users.noreply.github.com>
2026-02-28 09:06:13 -0500

    Initial commit

---
+++ src/AwsCdkCSharpStudy/AwsCdkCSharpStudyStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsCdkCSharpStudy
+{
+    public class AwsCdkCSharpStudyStack : Stack
+    {
+        internal AwsCdkCSharpStudyStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
